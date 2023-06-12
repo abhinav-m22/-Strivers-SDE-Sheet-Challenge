9e079ec240373eff21aa44a2fae3e0a76a125dab
abhinav-m22 <abhinavpm05@gmail.com>
2023-06-12 23:33:43 +0530

    Added Q22 solution

---
+++ 22_LongestSubarray0.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+int LongestSubsetWithZeroSum(vector<int> arr)
+{
+
+    // Brute force
+    /* int n = arr.size();
+    int maxi = 0;
+
+    for (int i = 0; i < n; i++) {
+      int sum = 0;
+      for (int j = i; j < n; j++) {
+        sum += arr[j];
+        if (sum == 0)
+          maxi = max(maxi, j - i + 1);
+      }
+    }
+
+    return maxi;
+    */
+
+   // Optimal
+    unordered_map<int, int> mp;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        arr[i] += arr[i - 1];
+    }
+
+    int maxi = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == 0)
+            maxi = i + 1;
+        else if (mp.find(arr[i]) != mp.end())
+        {
+            maxi = max(maxi, i - mp[arr[i]]);
+        }
+        else
+        {
+            mp[arr[i]] = i;
+        }
+    }
+
+    return maxi;
+}
