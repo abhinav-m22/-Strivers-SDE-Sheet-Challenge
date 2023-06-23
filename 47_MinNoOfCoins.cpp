f46049fcf36035075c100230e108e5394dc23d1c
abhinav-m22 <abhinavpm05@gmail.com>
2023-06-23 22:21:13 +0530

    Added Q47 solution

---
+++ 47_MinNoOfCoins.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMinimumCoins(int amount)
+{
+    int ans = 0;
+    vector<int> coins = {1000, 500, 100, 50, 20, 10, 5, 2, 1};
+
+    while (amount)
+    {
+        for (auto coin : coins)
+        {
+            if (amount >= coin)
+            {
+                amount -= coin;
+                break;
+            }
+        }
+        ans++;
+    }
+    return ans;
+}
