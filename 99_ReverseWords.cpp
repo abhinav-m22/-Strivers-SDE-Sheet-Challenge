c9269698c1f0c0af84f6283fc7fba9e6337231c6
abhinav-m22 <abhinavpm05@gmail.com>
2023-07-15 23:23:35 +0530

    Added Q99 solution

---
+++ 99_ReverseWords.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseString(string &s)
+{
+    // Write your code here.
+    if (s.size() == 0)
+        return s;
+
+    stack<string> st;
+    string ans;
+    for (int i = 0; i < s.size(); i++)
+    {
+        string word = "";
+        if (s[i] == ' ')
+
+            continue;
+
+        while (i < s.size() && s[i] != ' ')
+        {
+
+            word += s[i];
+
+            i++;
+        }
+
+        st.push(word);
+    }
+
+    while (st.empty() == 0)
+
+    {
+
+        ans += st.top();
+
+        st.pop();
+
+        if (!st.empty())
+
+        {
+
+            ans += ' ';
+        }
+    }
+
+    return ans;
+}
