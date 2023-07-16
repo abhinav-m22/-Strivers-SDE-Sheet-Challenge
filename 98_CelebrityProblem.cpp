57e87500094736a09180e418976914abe2bd4615
abhinav-m22 <abhinavpm05@gmail.com>
2023-07-16 22:34:57 +0530

    Added Q98 solution

---
+++ 98_CelebrityProblem.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+        This is signature of helper function 'knows'.
+        You should not implement it, or speculate about its implementation.
+
+        bool knows(int A, int B);
+        Function 'knows(A, B)' will returns "true" if the person having
+        id 'A' know the person having id 'B' in the party, "false" otherwise.
+*/
+
+int findCelebrity(int n) {
+  stack<int> s;
+  for (int i = 0; i < n; i++) {
+    s.push(i);
+  }
+
+  while (s.size() > 1) {
+    int a = s.top();
+    s.pop();
+    int b = s.top();
+    s.pop();
+    if (knows(a, b))
+      s.push(b);
+    else
+      s.push(a);
+  }
+  int ans = s.top();
+  for (int i = 0; i < n; i++) {
+    if (i == ans) {
+      continue;
+    }
+
+    if (knows(ans, i)) {
+      return -1;
+    }
+    if (!knows(i, ans)) {
+      return -1;
+    }
+  }
+  return ans;
+}
